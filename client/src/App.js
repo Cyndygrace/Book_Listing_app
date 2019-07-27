0b2b38089e965d1249f81ca99fddd6b375108144
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-27 20:40:27 +0100

    imported the BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// Components
+import BookList from './Components/BookList'
 
 function App() {
   return (
     <div id="main">
       <h1>For Your Reading Pleasure</h1>
+      <BookList />
     </div>
   );
 }
