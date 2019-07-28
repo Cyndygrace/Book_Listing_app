63b6476ef52ff611dcf4404fd0dc43f56d2316b8
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-28 09:39:12 +0100

    import gql -  graphql to javascript parser

--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { gql } from 'apollo-boost'; //GraphQL to javascript parser
 
 
+
+
+
+// component
 function BookList() {
   return (
     <div>
-      <ul id='book-list'>
+      <ul id="book-list">
         <li>Book Name</li>
       </ul>
     </div>
   );
 }
 
-export default BookList;
+
