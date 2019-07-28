41ae9a9dbc651fae34fe59d96a403a0e6b49be34
Cynthia Iloekwe <cynthiailoekwe@gmail.com>
2019-07-28 21:44:40 +0100

    seperated queries into a new file and imported the query from the file

--- client/src/Components/AddBook.js
+++ client/src/Components/AddBook.js
@@ -1,23 +1,31 @@
 import React from 'react';
-import { gql } from 'apollo-boost'; //GraphQL to javascript parser
+// import { gql } from 'apollo-boost'; //GraphQL to javascript parser
 import { graphql } from 'react-apollo'; // binds apollo to react
+import {getAuthorsQuery} from '../Queries/queries'
 
 // This is a component that picks an author from a list and add a book written by that author.
 // define our book query
-const getAuthorsQuery = gql`
-  {
-    Authors {
-      name
-      id
-    }
-  }
-`;
 
+// Query moved to query file
 // component
 const AddBook = props => {
   // console.log(props)
+  function displayAuthors() {
+    var data = props.data;
+    if (data.loading) {
+      return <option disabled>Loading Authors...</option>;
+    } else {
+      return data.authors.map(author => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
+        );
+      });
+    }
+  }
   return (
-    <form id="add-book" action="">
+    <form id="add-book">
       <div className="field">
         <label htmlFor="">Book name</label>
         <input type="text" />
@@ -27,13 +35,14 @@ const AddBook = props => {
         <input type="text" />
       </div>
       <div className="field">
-        <label htmlFor="">Author</label>
-        <select name="" id="">
-          <option value="">Select author</option>
+        <label>Author</label>
+        <select>
+          <option>Select author</option>
+          {displayAuthors()}
         </select>
-        </div>
+      </div>
 
-        <button>+</button>
+      <button>+</button>
     </form>
   );
 };
--- client/src/Components/BookList.js
+++ client/src/Components/BookList.js
@@ -1,37 +1,37 @@
 import React from 'react';
-import { gql } from 'apollo-boost'; //GraphQL to javascript parser
+// import { gql } from 'apollo-boost'; //GraphQL to javascript parser
 import { graphql } from 'react-apollo'; // binds apollo to react
+import {getBookQuery} from '../Queries/queries'
 
 // define our book query
-const getBookQuery = gql`
-  {
-    books {
-      name
-      id
-    }
-  }
-`
+// const getBookQuery = gql`
+//   {
+//     books {
+//       name
+//       id
+//     }
+//   }
+// `
 
 // component
 const  BookList = props =>  {
   // console.log(props)
-  // function to output data to the screen
   function displayBooks(){
     var data = props.data //this contains the result from ther server
     if (data.loading) {
       return (<div>Loading books...</div>)
     } else {
-      return data.books.map(book =>{
+      return data.books.map(book => {
         return(
-          <li key = {book.id}>{book.name}</li>
-        )
+          <li key={book.id}>{book.name}</li>
+        );
       })
     }
   }
   return (
     <div>
       <ul id="book-list">
-        {displayBooks()}
+        <li>{displayBooks()}</li>
       </ul>
     </div>
   );
